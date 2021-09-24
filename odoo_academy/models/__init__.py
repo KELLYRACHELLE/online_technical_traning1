55fa5b6be9da360f9fe2633526ea5ae1973a7db3
KELLYRACHELLE <rachellegagoum@gmail.com>
2021-09-24 19:25:10 +0000

    [ADD] course Module

---
+++ odoo_academy/models/__init__.py
@@ -0,0 +1,3 @@
+# -- conding: utf-8 --
+
+from odoo import models, fields, api
---
+++ odoo_academy/models/course.py
@@ -0,0 +1,11 @@
+# -- conding: utf-8 --
+
+from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
+
+class Course(models.Model):
+    
+    _name = 'academy.course'
+    _description = 'Course Info'
+    
+    
